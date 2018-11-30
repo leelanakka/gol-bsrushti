7fcae02f1d1b61f9cf0aee3982a73d146a857a90
bsrushti <bsrushti@in-bsrushti.local>
2018-11-30 09:18:40 +0530

    Removed unwanted functions which was requiring from util.js

--- src/gameOfLife.js
+++ src/gameOfLife.js
@@ -1,20 +1,8 @@
 const {
   initialGrid,
-  isCoordinatesGreaterThanBoard,
   generateWorld,
-  predicate,
-  checkRangeForNegativeNumbers,
-  cartesian,
-  validNeighbors,
-  findingNeighbors,
-  checkForAlive,
-  totalAliveNeighbors,
-  checkForNextGenration,
   generateNextWorld,
   getAliveCellsOfNextGeneration,
-  fillArray,
-  makeCounterFromN,
-  getCoordinates,
   filterInputs,
   cellCoordinates,
   getDimension
@@ -28,6 +16,6 @@ const nextGeneration = function(currGeneration,bounds) {
   let grid = generateWorld(initialGrid(width,height),filteredCurrentGeneration);
   let nextWorld = generateNextWorld(grid);
   return getAliveCellsOfNextGeneration(nextWorld, height, width).map(cell => [cell[0]+topLeft[0],cell[1]+topLeft[1]]);
-}
+};
 
 module.exports = { nextGeneration };
